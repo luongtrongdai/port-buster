4d6751ec5e4fdd2d31c4b6742f1c4164710fcceb
ltdai91 <ltdai91@gmail.com>
2014-08-13 07:40:15 +0000

    create url

---
+++ port-buster/src/main/java/com/port/entity/EntBillInfo.java
@@ -0,0 +1,18 @@
+package com.port.entity;
+
+public class EntBillInfo {
+	private String code;
+	private String type;
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+}
