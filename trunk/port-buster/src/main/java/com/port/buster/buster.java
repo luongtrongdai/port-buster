f3d9a7b8a49a7007839845cb5e21fc955043a83a
ltdai91@gmail.com <ltdai91@gmail.com@afb09db3-6cfc-00bc-0520-801aae902153>
2014-08-13 10:05:58 +0000

    Finish read content

--- trunk/port-buster/src/main/java/com/port/buster/buster.java
+++ trunk/port-buster/src/main/java/com/port/buster/buster.java
@@ -3,6 +3,7 @@ package com.port.buster;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,6 +12,8 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.MappingIterator;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
@@ -26,77 +29,11 @@ public class buster {
 	
 	public static void main(String[] args) {
 		try {
-			CsvSchema bootstrap = CsvSchema.builder()
-						.addColumn("code")
-						.addColumn("type")
-						.build();
-	
-			bootstrap =	bootstrap
-								.withSkipFirstDataRow(true)
-								.withoutHeader();
-	
-			CsvMapper csvMapper = new CsvMapper();
-			File csvFile = new File("info.csv");
-			MappingIterator<EntBillInfo> mappingIterator = csvMapper.reader(EntBillInfo.class).with(bootstrap).readValues(csvFile);
-			List<EntBillInfo>  lstBill = mappingIterator.readAll();
+			List<EntBillInfo>  lstBill = readListBillFromCSV();
 			
-			List<EntBillResult> lstResult = new ArrayList<EntBillResult>();
-			EntBillResult entResult;
-			String date;
-			String time;
-			String status;
-			String location;
-			for (EntBillInfo bill : lstBill) {
-				entResult = new EntBillResult();
-				entResult.setCode(bill.getCode());
-				
-				
-				String url = String.format(URL_FORMAT, bill.getCode()
-													 , EnumType.getTypeByKey(bill.getType()));
-				Document doc = Jsoup.connect(url).get();
-				Elements e = doc.select(".mGrid tr:last-child td");
+			List<EntBillResult> lstResult = parseResultByJSoup(lstBill);
 
-				date = CANNOT_FOUNT;
-				time = CANNOT_FOUNT;
-				status = CANNOT_FOUNT;
-				location = CANNOT_FOUNT;
-				
-				if (e != null && e.size() > 0) {
-					date = e.get(1).text();
-					time = e.get(2).text();
-					status = e.get(3).text();
-					location = e.get(4).text();
-				}
-				entResult.setDate(date);
-				entResult.setTime(time);
-				entResult.setLocation(location);
-				entResult.setStatus(status);
-				lstResult.add(entResult);
-			}
-			//Write csv file
-			File tempFile = new File("result.csv");
-	        //Create scheame
-	        FileOutputStream tempFileOutputStream = new FileOutputStream(tempFile);
-	        BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(tempFileOutputStream, 2048);
-	        OutputStreamWriter writerOutputStream = new OutputStreamWriter(bufferedOutputStream, "UTF-8");
-	        
-	        CsvSchema write = CsvSchema.builder()
-												.addColumn("code")
-												.addColumn("date")
-												.addColumn("time")
-												.addColumn("status")
-												.addColumn("location")
-												.build();
-	        write = write.withHeader();
-	        CsvMapper csvWrite = new CsvMapper();
-	        ObjectWriter myObjectWriter = csvWrite.writer(write);
-
-	        //Write list data to error file
-	        myObjectWriter.writeValue(writerOutputStream, lstResult);
-	        //Close stream
-	        tempFileOutputStream.close();
-	        bufferedOutputStream.close();
-	        writerOutputStream.close();
+			writeCSVResult(lstResult);
 	        
 			System.out.println("Done");
 		} catch (Exception e) {
@@ -104,4 +41,86 @@ public class buster {
 		}
 	}
 
+	private static List<EntBillInfo> readListBillFromCSV() throws IOException {
+		CsvSchema bootstrap = CsvSchema.builder()
+						.addColumn("code")
+						.addColumn("type")
+						.build();
+		
+		bootstrap =	bootstrap
+							.withSkipFirstDataRow(true)
+							.withoutHeader();
+	
+		CsvMapper csvMapper = new CsvMapper();
+		File csvFile = new File("info.csv");
+		MappingIterator<EntBillInfo> mappingIterator = csvMapper.reader(EntBillInfo.class).with(bootstrap).readValues(csvFile);
+		List<EntBillInfo>  lstBill = mappingIterator.readAll();
+		
+		return lstBill;
+	}
+	
+	private static List<EntBillResult> parseResultByJSoup(List<EntBillInfo> lstBillInfo) throws IOException {
+		List<EntBillResult> lstResult = new ArrayList<EntBillResult>();
+		EntBillResult entResult;
+		String date;
+		String time;
+		String status;
+		String location;
+		for (EntBillInfo bill : lstBillInfo) {
+			entResult = new EntBillResult();
+			entResult.setCode(bill.getCode());
+			
+			
+			String url = String.format(URL_FORMAT, bill.getCode()
+												 , EnumType.getTypeByKey(bill.getType()));
+			Document doc = Jsoup.connect(url).get();
+			Elements e = doc.select(".mGrid tr:last-child td");
+
+			date = CANNOT_FOUNT;
+			time = CANNOT_FOUNT;
+			status = CANNOT_FOUNT;
+			location = CANNOT_FOUNT;
+			
+			if (e != null && e.size() > 0) {
+				date = e.get(1).text();
+				time = e.get(2).text();
+				status = e.get(3).text();
+				location = e.get(4).text();
+			}
+			entResult.setDate(date);
+			entResult.setTime(time);
+			entResult.setLocation(location);
+			entResult.setStatus(status);
+			lstResult.add(entResult);
+		}
+		
+		return lstResult;
+	}
+	
+	private static void writeCSVResult(List<EntBillResult> lstResult) throws JsonGenerationException, JsonMappingException, IOException {
+		//Write csv file
+		File tempFile = new File("result.csv");
+        //Create scheame
+        FileOutputStream tempFileOutputStream = new FileOutputStream(tempFile);
+        BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(tempFileOutputStream, 2048);
+        OutputStreamWriter writerOutputStream = new OutputStreamWriter(bufferedOutputStream, "UTF-8");
+        
+        CsvSchema write = CsvSchema.builder()
+											.addColumn("code")
+											.addColumn("date")
+											.addColumn("time")
+											.addColumn("status")
+											.addColumn("location")
+											.build();
+        write = write.withHeader();
+        CsvMapper csvWrite = new CsvMapper();
+        ObjectWriter myObjectWriter = csvWrite.writer(write);
+
+        //Write list data to error file
+        myObjectWriter.writeValue(writerOutputStream, lstResult);
+        //Close stream
+        tempFileOutputStream.close();
+        bufferedOutputStream.close();
+        writerOutputStream.close();
+	}
 }
